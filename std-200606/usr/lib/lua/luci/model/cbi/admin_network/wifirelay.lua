8a33a6bbc97c595e1ebeb4c852f7dccf83595810
xfwu <757694219@qq.com>
2020-09-11 17:36:31 +0800

    1.fix wifidog show windows

--- std-200606/usr/lib/lua/luci/model/cbi/admin_network/wifirelay.lua
+++ std-200606/usr/lib/lua/luci/model/cbi/admin_network/wifirelay.lua
@@ -1,19 +1,19 @@
 
-m = Map("wifirelay", translate("WiFi Relay"),translate("Connect to upper AP Router"))
+m = Map("wifirelay", translate("连接WiFi"),translate("设置名称和密码连接可上网的WiFi"))
 
 s = m:section(NamedSection, "wifirelay2g", translate("Wifi Relay"))
 s.anonymous = true
 
 s:option(Flag, "enabled", translate("Enable"))
 
-s:option(Value, "ssid", translate("Upper SSID")).rmempty = true
-s:option(Value, "key", translate("PassWord")).rmempty = true
+s:option(Value, "ssid", translate("WiFi名称")).rmempty = true
+s:option(Value, "key", translate("WiFi密码")).rmempty = true
 
-v0 = s:option(ListValue, "AuthMode", translate("Auth Mode"))
+v0 = s:option(ListValue, "AuthMode", translate("验证方式"))
   v0:value("WPA2PSK", translate("WPA2PSK"))
   v0:value("OPEN", translate("OPEN"))
 
-v1 = s:option(ListValue, "EncrypType", translate("Encryp Type"))
+v1 = s:option(ListValue, "EncrypType", translate("加密方式"))
   v1:value("TKIPAES", translate("TKIPAES"))
   v1:value("AES", translate("AES"))
   v1:value("TKIP", translate("TKIP"))
